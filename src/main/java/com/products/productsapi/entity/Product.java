ec4e64734af16df07a04064667e7a0436d9bcc28
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-07-25 02:24:37 -0300

    Implements class that to do mapping of the database

---
+++ src/main/java/com/products/productsapi/entity/Product.java
@@ -0,0 +1,47 @@
+package com.products.productsapi.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Product {
+
+	@Id
+	private Integer id;
+	private String nome;
+	private int quantidade;
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+}
